1867fe0b818c950cfc842961234d64854bdee93d
yashparmar25 <yashparmar2055@gmail.com>
2026-04-08 17:21:04 +0530

    some important change

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -16,6 +16,7 @@ const Appointment = () => {
     const [slotIndex, setSlotIndex] = useState(0);
     const [slotTime, setSlotTime] = useState('');
 
+    
     // Fetch doctor info
     const fetchDocInfo = () => {
         const doc = doctors.find((doc) => doc._id === docId);
@@ -92,7 +93,7 @@ const Appointment = () => {
 
        const slotDate = `${day}-${month}-${year}`;
         
-       const {data} = await axios.post('http://localhost:4000/api/user/book-appointment',{docId,slotDate,slotTime},{headers:{token}});
+       const {data} = await axios.post(backendUrl + '/api/user/book-appointment',{docId,slotDate,slotTime},{headers:{token}});
        if(data.success){
         toast.success('Appointment booked successfully');
         getDoctorsData();
